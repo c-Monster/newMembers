89c83904597a205a569619b5d952d74a571718de
Clark Richard Green <clark.r.green@gmail.com>
2017-07-30 18:39:24 -0600

    started scaling

--- bishops.py
+++ bishops.py
@@ -11,47 +11,59 @@ UNIT_ADDRESS = 'https://www.lds.org/mls/mbr/services/cdol/details/unit/%(unit)s?
 PROFILE_ADDRESS = 'https://www.lds.org/mls/mbr/records/member-profile/service/M00%(mrn)s?lang=eng'
 HEADERS = {
         'accept':'application/json, text/plain, */*',
-        'accept-encoding':'gzip, dseflate, br',   
-        'accept-language':'en-US,en;q=0.8',
-        'content-type':'application/json;charset=UTF-8',
-        'origin':'https://www.lds.org',                                                                                   
-        'referer':'https://www.lds.org/mls/mbr/records/request/find-member',
         }
+
+#data will be a string of MRNs delineated by whitespace
+def getFormerBishopInfo(data, session, credentials)
+    
+    output
+    mrnList = data.split(" ")
+
+    for mrn in mrnList:
+
+        session = requests.session()
+        credentials = tools.login(session, username, password)
+        
+        address = PROFILE_ADDRESS % {'mrn': mrn}
+        response = session.get(url = address, cookies = credentials, headers = HEADERS)
+        assert response.status_code == 200
+        
+        try:
+            decoded = json.loads(response.text)
+        
+        except(ValueError, KeyError, TypeError):
+            print "Error parsing json reponse to profile request"
+        
+        print decoded['individual']['priorUnits'][0]['unitName']
+        
+        address = UNIT_ADDRESS % {'unit': decoded['individual']['priorUnits'][0]['unitNumber']}
+        response = session.get(url = address, cookies = credentials, headers = HEADERS)
+        assert response.status_code == 200
+        
+        try:
+            decoded = json.loads(response.text)
+            
+        except(ValueError, KeyError, TypeError):
+            print "Error parsing json response to unit request"
+        
+        print decoded['leaderName']
+        print decoded['leaderEmail']
+
+        output.join(decoded['leaderName'])
+        output.join(',')
+        output.join(decoded['leaderEmail'])
+        output.join('\n')
+
+    return output
+
+        
 
 username = sys.argv[1]
 password = sys.argv[2]
-
-mrn = raw_input("MRN: ")
+data = sys.stdin.read()
 
 session = requests.session()
 credentials = tools.login(session, username, password)
 
-address = PROFILE_ADDRESS % {'mrn': mrn}
-print address
-response = session.get(url = address, cookies = credentials, headers = HEADERS)
-print response.status_code
-assert response.status_code == 200
-
-try:
-    decoded = json.loads(response.text)
-
-except(ValueError, KeyError, TypeError):
-    print "Error parsing json reponse to profile request"
-
-print decoded['individual']['priorUnits'][0]['unitName']
-print decoded['individual']['priorUnits'][0]['unitNumber']
-
-address = UNIT_ADDRESS % {'unit': decoded['individual']['priorUnits'][0]['unitNumber']}
-response = session.get(url = address, cookies = credentials, headers = HEADERS)
-assert response.status_code == 200
-
-try:
-    decoded = json.loads(response.text)
-    
-except(ValueError, KeyError, TypeError):
-    print "Error parsing json response to unit request"
-
-print decoded['leaderName']
-print decoded['leaderEmail']
-
-
+output = getFormerBishopInfo(data, session, credentials)
+print output
